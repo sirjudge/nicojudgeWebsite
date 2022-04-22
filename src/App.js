8f754aa09262f2be52f9462b49da32ee9b54ee21
nico <nico.a.judge@gmail.com>
2022-04-22 15:10:21 -0400

    changed default react layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { data } from "./gitProjects"
+
+function Header(){
+    return (
+        <div>
+            <h1>Nico Judge</h1>
+            <h4>Baltimore, MD</h4>
+            <table>
+                <tr>
+                    <td>
+                        www.github.com/sirjudge
+                    </td>
+                    <td>
+                        nico.a.judge@gmail.com
+                    </td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+function RepoList(repos){
+    var gitData = GetGitJson();
+    return (
+        data.map(function(repo){
+              const { id,url,name} = repo;
+              return (
+                  <tr>
+                      <td>{name}</td>
+                      <td><a href={url}>{url}</a></td>
+                  </tr>
+              )
+        })
+    )
+}
+
+function GetGitJson(){
+    var xhr = new XMLHttpRequest();
+    var url = "https://api.github.com/users/sirjudge/repos?callback=CALLBACK";
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            return json;
+        }
+    };
+    xhr.send();
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Nico Judge
-            Under Construction and coming soon
-        </p> 
-      </header>
+        <header className="App-header">
+          <Header></Header>
+        </header>
+        <table>
+            <tbody>
+                <RepoList></RepoList>
+            </tbody>
+        </table>
      </div>
   );
 }
---
+++ src/gitProjects.js
@@ -0,0 +1,5 @@
+export const data =[
+    {id: 1,url:"https://github.com/sirjudge/nicojudgeWebsite",name:"reactWebsite"},
+    {id: 2,url:"https://github.com/sirjudge/tacBot",name:"TacBot"},
+    {id: 2,url:"https://github.com/sirjudge/tacBot",name:"TacBot"}
+]
