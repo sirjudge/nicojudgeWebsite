aa4ba4b9cb7ba87115fe86c976c5c778af0b40ab
nico <nico.a.judge@gmail.com>
2022-04-25 22:23:40 -0400

    added logic to retrieve github links from xmlHttpRequest

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { data } from "./gitProjects"
 function Header(){
     return (
         <div>
-            <h1>Nico Judge</h1>
+            <h1 id="nameHeader">Nico Judge</h1>
             <h4>Baltimore, MD</h4>
             <table>
                 <tr>
@@ -36,18 +36,62 @@ function RepoList(repos){
     )
 }
 
-function GetGitJson(){
-    var xhr = new XMLHttpRequest();
-    var url = "https://api.github.com/users/sirjudge/repos?callback=CALLBACK";
-    xhr.open("GET", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-            return json;
-        }
-    };
+//TODO: This is stack overflow code. This does the await
+//https://stackoverflow.com/questions/48969495/in-javascript-how-do-i-should-i-use-async-await-with-xmlhttprequest
+function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+function GetGitJsonXhrRequest(){
+    const url = "https://api.github.com/users/sirjudge/repos?callback=CALLBACK";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('get',url);
     xhr.send();
+
+    var returnJson;
+    xhr.onload = function() {
+        returnJson = JSON.parse( xhr.response);
+    };
+
+    while (returnJson === null){
+
+    }
+}
+
+function GetGitJson(){
+    var url = "https://api.github.com/users/sirjudge/repos?callback=CALLBACK";
+    fetch(url,
+        {
+            method: "GET"
+        })
+        .then(function(res){
+
+            console.log(res.json());
+            return res.json();
+
+        })
+        .then(function(data){ console.log(( JSON.stringify( data ) ) )})
 }
 
 function App() {
