5d29ed9aabd849333b29325b4cfe8d1d3e30f630
nico <nico.a.judge@gmail.com>
2022-05-15 21:31:11 -0400

    changed repoList function to return the entire table rather than list of table rows

--- src/GetData.js
+++ src/GetData.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { data } from "./gitProjects"
 
 function Header(){
     return (
@@ -10,7 +8,7 @@ function Header(){
             <table>
                 <tr>
                     <td>
-                        www.github.com/sirjudge
+                        <a href="www.github.com/sirjudge">www.github.com/sirjudge</a>
                     </td>
                     <td>
                         nico.a.judge@gmail.com
@@ -21,62 +19,40 @@ function Header(){
     )
 }
 
-function RepoList(repos){
-    var gitData = GetGitJson();
-    return (
-        data.map(function(repo){
-              const { id,url,name} = repo;
-              return (
-                  <tr>
-                      <td>{name}</td>
-                      <td><a href={url}>{url}</a></td>
-                  </tr>
-              )
-        })
-    )
-}
-
-//TODO: This is stack overflow code. This does the await
-//https://stackoverflow.com/questions/48969495/in-javascript-how-do-i-should-i-use-async-await-with-xmlhttprequest
-function makeRequest(method, url) {
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            }
-        };
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: xhr.statusText
-            });
-        };
-        xhr.send();
-    });
-}
-
-// Example POST method implementation:
 async function GetData(url = '', data = {}) {
     const response = await fetch(url, {
         method: 'GET'
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+    return response.json();// parses JSON response into native JavaScript objects
 }
 
-ExtractGitRepoList(){
+// async function ExtractGitRepoList(){
+async function RepoList(){
     var url="https://api.github.com/users/sirjudge/repos";
-    GetData(url)
+    var repoData;
+    await GetData(url)
         .then(data => {
-
-            console.log(data); // JSON data parsed by `data.json()` call
+            repoData = data;
         });
+
+    return (
+        <table>
+            <tbody>
+            {repoData.map(repo => {
+                    return (
+                    <tr key={repo.id}>
+                        <td>
+                            {repo.name}
+                        </td>
+                        <td>
+                            <a href={repo.url}></a>
+                        </td>
+                    </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    );
 }
 
 function App() {
@@ -85,11 +61,7 @@ function App() {
         <header className="App-header">
           <Header></Header>
         </header>
-        <table>
-            <tbody>
-                <RepoList></RepoList>
-            </tbody>
-        </table>
+        <RepoList></RepoList>
      </div>
   );
 }
