37b97aaf8a1e5f5f3bf4f2995f02c00f60b6ce93
nico <nico.a.judge@gmail.com>
2022-05-22 21:59:35 -0400

    replaced header links with font awesome iocns

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import './gitProjects'
 import {RepoData} from "./gitProjects";
 import moment from "moment";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 function Header(){
     return (
@@ -11,10 +13,13 @@ function Header(){
             <table>
                 <tr>
                     <td>
-                        <a href="www.github.com/sirjudge">www.github.com/sirjudge</a>
+                        <a href="www.github.com/sirjudge"><FontAwesomeIcon icon={brands("github")}/></a>
                     </td>
                     <td>
-                        nico.a.judge@gmail.com
+                        <a href="mailto:nico.a.judge@gmail.com"><FontAwesomeIcon icon={solid("envelope")}/></a>
+                    </td>
+                    <td>
+                        <a href="https://www.linkedin.com/in/nicojudge/"><FontAwesomeIcon icon={brands("linkedin")}/></a>
                     </td>
                 </tr>
             </table>
