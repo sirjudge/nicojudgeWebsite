56454a515129b184c082266eb28b0eb703ce077a
nico <nico.a.judge@gmail.com>
2024-03-18 23:46:04 -0400

    did more layout updating and flushed out repo page

--- src/src/repos.rs
+++ src/src/repos.rs
@@ -1,38 +1,42 @@
 use leptos::*;
+use std::env;
 
+struct repo {
+    name: String,
+    html_url: String,
+}
 
 #[component]
 pub fn Repos() -> impl IntoView {
     let repos = get_repos();
-    view! {
-        <div id="repos">
-            <h1>"These are my repos!"</h1>
-            <p>
-                These are my repos 
-            </p> 
-            <ul>
-                {match repos {
-                    Ok(repos) => {
-                        ReposLoaded(repos)
-                    }
-                    Err(_) => {
-                        reposNotLoaded()
-                    }
-                }}
-            </ul>
-        </div>
+    if repos.is_empty() {
+        view! {
+        
+        }
+    } 
+    else {
+        view! {
+        }
+
     }
 }
 
-fn get_repos() -> impl Future<Item = Vec<repo>, Error = ()> {
+fn get_repos() -> Vec<repo> {
+   let repos = vec![
+        repo {
+            name: "leptos".to_string(),
+            html_url: "www.github.com/sirjudge".to_string)
+    /* 
     let url = "https://api.github.com/users/nicojudge/repos";
     let request = Request::new(url);
-    let auth_token = format!("bearer {}", env!("GITHUB_AUTH_TOKEN"));
+    let auth_token = format!("bearer {}",std::env!("GITHUB_AUTH_TOKEN"));
     request.headers().set("Authorization", auth_token);
     let future = fetch(request)
         .and_then(|response| response.json::<Vec<repo>>())
         .map_err(|_| ());
-    future
+    let repos = futures::executor::block_on(future);
+  */ 
+    repos
 }
 
 #[component]
