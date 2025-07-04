cb4e6dcff1577beabcb7ad64030bade9d4554918
Nico Judge <nico.a.judge@gmail.com>
2025-07-04 17:23:01 -0400

    cleaned up unused imports + fixed some compilation warnings

--- flake.nix
+++ flake.nix
@@ -13,13 +13,12 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-# To allow cursor to be used
-
     flake-utils.lib.eachDefaultSystem (system:
       # Define the variables to use in the in { ... } section
       let
         # Use a pre-built rust environment as a wrapper
         overlays = [ (import rust-overlay) ];
+
         # Shorthand the nix packages library
         pkgs = import nixpkgs {
             inherit system overlays;
@@ -27,6 +26,7 @@
                 allowUnfree = true;
             };
         };
+
         # Define which tools in the rust tool chain to use
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" "clippy" ];
@@ -34,34 +34,27 @@
         };
       in {
 
-        # When a dev shell is launched, create a package config with the following nixPackages.
-        #
+        # Load packages used for build time
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ pkg-config ];
           buildInputs = with pkgs; [
+            # Build dependencies
             rustToolchain
             clang
             llvmPackages_latest.bintools
             glibc.dev
             glib.dev
-            bacon
             openssl
             cargo-binstall
             sqlite
-            # udev
-            # vulkan-loader
-            # xorg.libX11
-            # xorg.libXcursor
-            # xorg.libXi
-            # xorg.libXrandr
-            # wayland
-            # libxkbcommon
+            # Load bacon, a rust CLI tool for convience
+            bacon
           ];
 
          # The following is executed once after a shell is launched but before
          # The use can input a command
           shellHook = ''
-            # Define cargo and library paths
+            # Define cargo and library paths for pkgconfig
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
             export LD_LIBRARY_PATH=${
               pkgs.lib.makeLibraryPath [
@@ -77,10 +70,9 @@
 
             # Set up the build environment
             export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.glibc.dev}/include -I${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include -I${pkgs.glib.dev}/include/glib-2.0 -I${pkgs.glib.out}/lib/glib-2.0/include/";
-            # Only use lld for non-wasm targets
-            # if [ "$CARGO_BUILD_TARGET" != "wasm32-unknown-unknown" ]; then
-            #   export RUSTFLAGS="-C link-arg=-fuse-ld=lld";
-            # fi
+
+            # Start ZSH shell instead of bash shell
+            zsh
           '';
         };
       });
--- source/src/views/admin.rs
+++ source/src/views/admin.rs
@@ -1,18 +1,20 @@
-use dioxus::prelude::*;
-use crate::components::{
-    AdminLogin,
-    AdminView,
+use crate::components::{AdminLogin, AdminView};
+use dioxus::{
+    logger::tracing::{error, info, warn},
+    prelude::*,
 };
-
 
 // TODO: obviously this is not secure, I'll be coming back to this later
 // and adding proper auth and session managment later
 // but don't need to worry about that when I have no actual functionality
+// TODO: Should also log bad login attempts as well
 #[server]
 pub async fn validate_login(username: String, password: String) -> Result<bool, ServerFnError> {
     if username == "admin" && password == "password" {
+        info!("Admin login successful");
         Ok(true)
     } else {
+        warn!("Admin login failed for user: {}", username);
         Ok(false)
     }
 }
@@ -27,7 +29,7 @@ pub async fn validate_session() -> Result<bool, ServerFnError> {
 #[component]
 pub fn Admin() -> Element {
     let session_valid: Option<bool> = Some(true);
-    //TODO: This be not working, just set to true for now and figure this part out later
+    //BUG: This be not working, just set to true for now and figure this part out later
     // use_server_future(|| async {
     //     session_valid = Some(validate_session().await.unwrap());
     // });
@@ -36,10 +38,12 @@ pub fn Admin() -> Element {
         Some(session_valid) => {
             // if session isn't valid, return to login page
             if !session_valid {
-                return rsx! { AdminView {} };
+                warn!("Admin session is not valid, redirecting to login page");
+                return rsx! { AdminLogin {} };
             }
 
-            return rsx! { AdminLogin {} };
+            info!("Admin session is valid, rendering admin view");
+            return rsx! { AdminView {} };
         }
         None => {
             // If we haven't validated yet display "loading"
@@ -50,4 +54,3 @@ pub fn Admin() -> Element {
         }
     };
 }
-
