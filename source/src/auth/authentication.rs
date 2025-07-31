384e2b39c677f83ba72138956db562ee61cf6120
Nico Judge <nico.a.judge@gmail.com>
2025-07-30 20:13:54 -0400

    hooked up password hashing to login validation

--- source/src/auth/authentication.rs
+++ source/src/auth/authentication.rs
@@ -6,14 +6,9 @@ use password_hash::SaltString;
 // #[cfg(feature = "server")]
 // use rand::rngs::OsRng;
 #[cfg(feature = "server")]
-use argon2::{
-    password_hash::Salt,
-    Argon2,
-    PasswordHasher,
-    PasswordVerifier
-};
+use argon2::{password_hash::Salt, Argon2, PasswordHasher, PasswordVerifier};
 use dioxus::{
-    logger::tracing::{info, warn},
+    logger::tracing::{error, info, warn},
     prelude::*,
 };
 use serde::{Deserialize, Serialize};
@@ -33,10 +28,32 @@ pub async fn validate_session() -> Result<bool, ServerFnError> {
 // TODO: Should also log bad login attempts as well
 #[server]
 pub async fn validate_login(username: String, password: String) -> Result<bool, ServerFnError> {
-    let account = get_account_by_username(username);
-
-    //TODO: Final step should be to remove this
-    Ok(true)
+    match get_account_by_username(username)
+        .await
+        .expect("error retrieving account for login validation")
+    {
+        Some(account) => {
+            let password_hash = hash_password(password)
+                .await
+                .expect("Error hashing password for login validation");
+            if password_hash == account.password_hash {
+                info!("Passwords match yippee");
+                Ok(true)
+            } else {
+                error!(
+                    "Passwords don't match. new hash:{} Saved hash:{}",
+                    password_hash, account.password_hash
+                );
+                Ok(false)
+            }
+        }
+        None => {
+            //TODO: Should handle this a bit better, fine to keep as
+            //error message for now
+            error!("account not found in lookup to validate");
+            Ok(false)
+        }
+    }
 }
 
 #[server]
@@ -46,10 +63,6 @@ pub async fn hash_password(password: String) -> Result<String, ServerFnError> {
             "input password is empty when it should not be",
         ));
     }
-
-    // Use salt from environment variable
-    // TOO: This is bad?
-
     //TODO: RustCrypto suggests using this but it doesn't
     //specify where OsRng comes from . . . deal with it later and instead use env variable
     //for now
@@ -58,16 +71,13 @@ pub async fn hash_password(password: String) -> Result<String, ServerFnError> {
     let salt = std::env::var("DB_PWD_SALT")
         .map_err(|_| ServerFnError::new("DB_PWD_SALT environment variable not set"))?;
     let salt_string = SaltString::new(&salt).unwrap();
-    let pwd_hash = argon2.hash_password(password.as_bytes(), &salt_string).unwrap();
-    // This is the hash we will store. Notice our salt string is included, as well as parameters:
-    // version 0x13 (19), memory 19456KiB (19 MiB), 2 iterations (time), parallelism 1
-    // let expected =
-    //     "$argon2id$v=19$m=19456,t=2,p=1$YmFkIHNhbHQh$DqHGwv6NQV0VcaJi7jeF1E8IpfMXmXcpq4r2kKyqpXk";
-    //   ^ hash ^ parameters            ^ salt       ^ combined hash
-
-    // assert_eq!(expected, hash.to_string());
-
-    //The verifier reads the salt and the parameters from the hash and verifies the result is equal
+    let pwd_hash = argon2
+        .hash_password(password.as_bytes(), &salt_string)
+        .unwrap();
+    info!(
+        "Successfully hashed password:{} into hash:{}",
+        password, pwd_hash
+    );
     Argon2::default()
         .verify_password(password.as_bytes(), &pwd_hash)
         .expect("invalid password");
