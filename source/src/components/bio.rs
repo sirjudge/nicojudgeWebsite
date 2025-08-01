476d7f404a0cb22e9a60877ad6d3bc47775e7ec9
Nico Judge <nico.a.judge@gmail.com>
2025-07-31 20:13:42 -0400

    added new images for bio page and added asset feature to dioxus

--- source/src/components/bio.rs
+++ source/src/components/bio.rs
@@ -1,19 +1,37 @@
+use dioxus::html::image;
+use dioxus::logger::tracing::{debug, error, info, warn};
 use dioxus::prelude::*;
-use dioxus::logger::tracing::{info, warn, debug, error};
+
+const NICO_MOWER_PIC: Asset = asset!(
+    "/assets/images/nico_mower.jpg",
+    ImageAssetOptions::new()
+    .with_format(ImageFormat::Avif)
+    .with_size(ImageSize::Automatic)
+);
 
 #[component]
 pub fn Bio() -> Element {
     rsx! {
         div {
             class: "bio",
-            h2 { "About Me" }
-            p { "
-                Hello! I'm a fullstack software engineer with nearly a decade in
+            style: "display:flex; flex-direction:column;",
+            h2 { "About Me" },
+            img {
+                id: "bio_main_pic",
+                src: NICO_MOWER_PIC,
+                style: "display:block;max-width:30%;max-height:30%;width:auto;height:auto;"
+            },
+            p {
+                "Hello! I'm a fullstack software engineer with nearly a decade in
                 building high performant web applications, services, and tooling for
-                nearly a decade. The page you're viewing right now is built from
+                nearly a decade."
+            },
+            p {
+                "The page you're viewing right now is built from
                 the ground up with Rust and Dioxus, a modern web framework that
                 allows for building fast and efficient web applications with
-                the power of WASM" }
+                the power of WASM"
+            }
         }
     }
 }
